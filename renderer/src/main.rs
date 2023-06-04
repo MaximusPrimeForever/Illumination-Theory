9d8540a6149538f5eec83840b6dd508daf321ca8
Max <grachasaurus@gmail.com>
2023-06-04 09:59:16 +0300

    Complete Vec3 implementation. Replace hardcoded values in main.rs with values relative to image dimensions.

--- renderer/src/main.rs
+++ renderer/src/main.rs
@@ -7,11 +7,12 @@ fn main() {
 
     let img_width: i32 = 256;
     let img_height: i32 = 256;
+    let img_max_color: f64 = 255.0;
 
     // Render
 
     // Header
-    println!("P3\n{img_width} {img_height}\n255\n");
+    println!("P3\n{img_width} {img_height}\n{img_max_color}\n");
 
     let img_width_norm: f64 = f64::from(img_width - 1);
     let img_height_norm: f64 = f64::from(img_height - 1);
@@ -21,8 +22,8 @@ fn main() {
     
     // Pixels
     for i in 0..img_height {
-        tri_start = 127 - (i/2);
-        tri_end = 127 + (i/2);
+        tri_start = (img_width / 2) - (i/2);
+        tri_end = (img_width / 2) + (i/2);
         eprint!("\rScanlines remaining: {}", img_height-i);
         stderr.flush().unwrap();
 
@@ -33,16 +34,22 @@ fn main() {
 
             if j >= tri_start && j < tri_end {
                 r = f64::from(j) / img_width_norm;
-                g = f64::from(255 - i) / img_height_norm;
+                g = f64::from(img_height - i) / img_height_norm;
                 b = 0.25;
             }
 
-            let ir: u8 = (r * 255.0) as u8;
-            let ig: u8 = (g * 255.0) as u8;
-            let ib: u8 = (b * 255.0) as u8;
+            let ir: u8 = (r * img_max_color) as u8;
+            let ig: u8 = (g * img_max_color) as u8;
+            let ib: u8 = (b * img_max_color) as u8;
 
             println!("{ir} {ig} {ib}")
         }
     }
-    eprintln!("\nDone.")
+    eprintln!("\nDone.");
+
+    let v1: vec3::Vec3 = vec3::Vec3::origin();
+    let v2: vec3::Vec3 = vec3::Vec3::new(1.0, 2.0, 3.0);
+
+    eprintln!("v1: {} {} {}", v1[0], v1[1], v1[2]);
+    eprintln!("v2: {} {} {}", v2.x(), v2.y(), v2.z());
 }
--- renderer/src/vec3.rs
+++ renderer/src/vec3.rs
@@ -1,24 +1,30 @@
-use std::ops;
+use std::{ops};
 
-struct Vec3 {
+pub struct Vec3 {
     e: [f64; 3]
 }
 
 impl Vec3 {
-    fn origin() -> Vec3 {
-        Vec3{e: [0.0, 0.0, 0.0]}
-    }
+    pub fn origin() -> Vec3 { Vec3{e: [0.0, 0.0, 0.0]} }
+    pub fn new(e1: f64, e2: f64, e3: f64) -> Vec3 { Vec3{e: [e1, e2, e3]} }
 
-    fn new(e1: f64, e2: f64, e3: f64) -> Vec3 {
-        Vec3{e: [e1, e2, e3]}
+    pub fn x(&self) -> f64 { self.e[0] }
+    pub fn y(&self) -> f64 { self.e[1] }
+    pub fn z(&self) -> f64 { self.e[2] }
+
+    pub fn length(&self) -> f64 {
+        self.length_squared().sqrt()
+    }
+    pub fn length_squared(&self) -> f64 {
+        self.e[0] * self.e[0] + self.e[1] * self.e[1] + self.e[2] * self.e[2]
     }
 }
 
 impl ops::Index<usize> for Vec3 {
     type Output = f64;
 
-    fn index(&self, index: usize) -> Self::Output {
-        (*self).e[index]
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.e[index]
     }
 }
 
@@ -27,10 +33,35 @@ impl ops::Add<Vec3> for Vec3 {
 
     fn add(self, _rhs: Vec3) -> Self::Output {
         Vec3::new(
-            self.e1 + _rhs.e1,
-            self.e2 + _rhs.e2,
-            self.e3 + _rhs.e3
+            self[0] + _rhs[0],
+            self[0] + _rhs[0],
+            self[0] + _rhs[0]
         )
+    }
+}
+
+impl ops::AddAssign for Vec3 {
+    
+    fn add_assign(&mut self, rhs: Self) {
+        self.e[0] += rhs.e[0];
+        self.e[1] += rhs.e[1];
+        self.e[2] += rhs.e[2];
+    }
+}
+
+impl ops::MulAssign<f64> for Vec3 {
+    fn mul_assign(&mut self, rhs: f64) {
+        self.e[0] *= rhs;
+        self.e[1] *= rhs;
+        self.e[2] *= rhs;
+    }
+}
+
+impl ops::DivAssign<f64> for Vec3 {
+    fn div_assign(&mut self, rhs: f64) {
+        self.e[0] /= rhs;
+        self.e[1] /= rhs;
+        self.e[2] /= rhs;
     }
 }
 
@@ -38,9 +69,9 @@ impl ops::Neg for Vec3 {
     type Output = Vec3;
     fn neg(self) -> Self::Output {
         Vec3::new(
-            -self.e1,
-            -self.e2,
-            -self.e3
+            -self.e[0],
+            -self.e[1],
+            -self.e[2]
         )
     }
 }
