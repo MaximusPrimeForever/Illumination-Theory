a81442f9cef7dbd42c830799cc8906a558a2037a
Max <grachasaurus@gmail.com>
2023-06-06 23:29:32 +0300

    Add a bunch of operator overloaders.

--- renderer/src/vec3.rs
+++ renderer/src/vec3.rs
@@ -7,6 +7,7 @@ pub struct Vec3 {
 impl Vec3 {
     pub fn origin() -> Vec3 { Vec3{e: [0.0, 0.0, 0.0]} }
     pub fn new(e1: f64, e2: f64, e3: f64) -> Vec3 { Vec3{e: [e1, e2, e3]} }
+    pub fn unit() -> Vec3 { Vec3 { e: [1.0, 1.0, 1.0]} }
 
     pub fn x(&self) -> f64 { self.e[0] }
     pub fn y(&self) -> f64 { self.e[1] }
@@ -28,17 +29,6 @@ impl ops::Index<usize> for Vec3 {
     }
 }
 
-impl ops::Add<Vec3> for Vec3 {
-    type Output = Vec3;
-
-    fn add(self, _rhs: Vec3) -> Self::Output {
-        Vec3::new(
-            self[0] + _rhs[0],
-            self[0] + _rhs[0],
-            self[0] + _rhs[0]
-        )
-    }
-}
 
 impl ops::AddAssign for Vec3 {
     
@@ -76,3 +66,94 @@ impl ops::Neg for Vec3 {
     }
 }
 
+// Second part
+// ===========
+
+impl std::fmt::Display for Vec3 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {} {}", self.x(), self.y(), self.z())
+    }
+}
+
+impl ops::Add<Vec3> for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, rhs: Vec3) -> Self::Output {
+        Vec3::new(
+            self[0] + rhs[0],
+            self[0] + rhs[0],
+            self[0] + rhs[0]
+        )
+    }
+}
+
+impl ops::Sub<Vec3> for Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, rhs: Vec3) -> Self::Output {
+        Vec3::new(
+            self[0] - rhs[0],
+            self[0] - rhs[0],
+            self[0] - rhs[0]
+        )
+    }
+}
+
+impl ops::Mul<Vec3> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: Vec3) -> Self::Output {
+        Vec3::new(
+            self[0] * rhs[0],
+            self[0] * rhs[0],
+            self[0] * rhs[0]
+        )
+    }
+}
+
+// Huh, that's cool
+impl ops::Mul<Vec3> for f64 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: Vec3) -> Self::Output {
+        Vec3::new(
+            rhs[0] * self,
+            rhs[0] * self,
+            rhs[0] * self
+        )
+    }
+}
+
+impl ops::Mul<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        Vec3::new(
+            self[0] * rhs,
+            self[0] * rhs,
+            self[0] * rhs
+        )
+    }
+}
+
+impl ops::Div<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn div(self, rhs: f64) -> Self::Output {
+        (1.0 / rhs) * self
+    }
+}
+
+fn dot(u: Vec3, v: Vec3) -> f64 {
+      u.x() * v.x()
+    + u.y() * v.y()
+    + u.z() * v.z()
+}
+
+fn cross(u: Vec3, v: Vec3) -> Vec3 {
+    Vec3::new(
+        u.e[1] * v.e[2] - u.e[2] * v.e[1],
+        u.e[2] * v.e[0] - u.e[0] * v.e[2],
+        u.e[0] * v.e[1] - u.e[1] * v.e[0]
+    )
+}
