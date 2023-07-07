e727e69bb6885f310e87ff9282ab73d68721283d
MaximusPrimeForever <max.anisimov230@gmail.com>
2023-07-07 23:58:29 +0300

    Check if `eog` exists before using it in script.

--- render.sh
+++ render.sh
@@ -6,4 +6,12 @@ if [ $# -eq 0 ]; then
 fi
 
 RUSTFLAGS="--allow dead_code" cargo run --manifest-path=./tracer/Cargo.toml > $1.ppm
-eog $1.ppm 2> /dev/null
+
+if command -v eog >/dev/null 2>&1; then
+  eog $1.ppm 2> /dev/null
+else
+  echo "Looks like you're not using gnome."
+  echo "You can replace *eog* in this script with your preferred image viewer."
+  exit 1
+fi
+
