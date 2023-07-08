a4eec692df3359910e5e4cf49f60dbbe2cb9a371
MaximusPrimeForever <max.anisimov230@gmail.com>
2023-07-08 20:43:47 +0300

    Remove hit_sphere from Ray. Move hit_sphere doc to Sphere.

--- tracer/src/ray.rs
+++ tracer/src/ray.rs
@@ -23,36 +23,3 @@ impl Ray {
         self.origin + t * self.direction
     }
 }
-
-pub fn hit_sphere(center: Point3, radius: f64, ray: &Ray) -> f64 {
-    /* Compute intersection of given ray and given sphere.
-    
-    The sphere equation is:
-    (x - Cx)^2 + (y - Cy)^2 + (z - Cz)^2 = r^2
-    Given the sphere vector and a 3D point we can rewrite as:
-    (P - C)*(P - C) = r^2
-    
-    We want to check if a given ray has a 't' for which it intersects with the sphere.
-    So,
-    (P(t) - C)(P(t) - C) = r^2
-    (A + t*b - C)(A + t*b - C) = r^2
-    Simplify, and we get a quadratic equation:
-    t^2*b*b + t*2b*(A-C) + (A-C)*(A-C) - r^2 = 0
-
-    Solving for t (the only unknown) there is a square root part that is either:
-    * positive - we get 2 intersection points.
-    * zero - we get a single intersection point.
-    * negative - we don't get an intersection point.
-     */
-    let oc: Vec3 = ray.origin - center;
-    let a = ray.direction.length_squared();
-    let half_b: f64 = dot(&ray.direction, &oc);
-    let c: f64 = oc.length_squared() - radius * radius;
-    let discriminant: f64 = half_b * half_b - a * c;
-
-    if discriminant < 0.0 {
-        -1.0
-    } else {
-        (-half_b - discriminant.sqrt()) / a
-    }
-}
--- tracer/src/sphere.rs
+++ tracer/src/sphere.rs
@@ -15,6 +15,29 @@ impl Sphere {
 }
 
 impl HittableT for Sphere {
+    /// Compute intersection of given ray and given sphere.
+    ///
+    /// The sphere equation is:
+    /// 
+    /// (x - Cx)^2 + (y - Cy)^2 + (z - Cz)^2 = r^2
+    /// 
+    /// Given the sphere vector and a 3D point we can rewrite as:
+    /// 
+    /// (P - C)*(P - C) = r^2
+    /// 
+    /// We want to check if a given ray has a 't' for which it intersects with the sphere.
+    /// 
+    /// So,
+    /// (P(t) - C)(P(t) - C) = r^2
+    /// (A + t*b - C)(A + t*b - C) = r^2
+    /// 
+    /// Simplify, and we get a quadratic equation:
+    /// t^2*b*b + t*2b*(A-C) + (A-C)*(A-C) - r^2 = 0
+    /// 
+    /// Solving for t (the only unknown) there is a square root part that is either:
+    /// * positive - we get 2 intersection points.
+    /// * zero - we get a single intersection point.
+    /// * negative - we don't get an intersection point.
     fn hit(&self, ray: &Ray, t_min: f64, t_max: f64, rec: &mut HitRecord) -> bool {
         let oc: Vec3 = ray.origin - self.center;
         let a = ray.direction.length_squared();
