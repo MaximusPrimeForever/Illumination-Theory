9f286e7e4f235869a9587a595fc7727aa149ac71
Max <grachasaurus@gmail.com>
2023-10-02 00:35:28 +0300

    Add perlin noise parameters to constructor.

--- tracer/src/perlin.rs
+++ tracer/src/perlin.rs
@@ -3,16 +3,32 @@ use crate::vec3::Point3;
 
 pub struct Perlin {
     random_floats: Vec<f64>,
+    scale: f64,
     perm_x: Vec<u64>,
     perm_y: Vec<u64>,
     perm_z: Vec<u64>
 }
 
 impl Perlin {
+    pub fn new(point_count: usize, scale: f64) -> Self {
+        let mut rf = Vec::<f64>::with_capacity(point_count);
+        for _ in 0..point_count {
+            rf.push(rand::random::<f64>());
+        }
+
+        Perlin { 
+            random_floats: rf,
+            scale,
+            perm_x: Perlin::generate_perlin_permutation(point_count),
+            perm_y: Perlin::generate_perlin_permutation(point_count),
+            perm_z: Perlin::generate_perlin_permutation(point_count)
+        }
+    }
+
     pub fn noise(&self, point: &Point3) -> f64 {
-        let i = ((4.0 * point.x()) as i64 & 255) as usize;
-        let j = ((4.0 * point.y()) as i64 & 255) as usize;
-        let k = ((4.0 * point.z()) as i64 & 255) as usize;
+        let i = ((self.scale * point.x()) as i64 & (self.random_floats.len() - 1) as i64) as usize;
+        let j = ((self.scale * point.y()) as i64 & (self.random_floats.len() - 1) as i64) as usize;
+        let k = ((self.scale * point.z()) as i64 & (self.random_floats.len() - 1) as i64) as usize;
 
         let index = (self.perm_x[i] ^ self.perm_y[j] ^ self.perm_z[k]) as usize;
         return self.random_floats[index]
@@ -47,6 +63,7 @@ impl Default for Perlin {
 
         Perlin { 
             random_floats: rf,
+            scale: 4.0,
             perm_x: Perlin::generate_perlin_permutation(point_count),
             perm_y: Perlin::generate_perlin_permutation(point_count),
             perm_z: Perlin::generate_perlin_permutation(point_count)
